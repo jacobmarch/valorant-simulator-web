a93ca461983b7c975c790d3599bea12c3e7b6a0e
Jacob Marchywka <dcfalcons2021@gmail.com>
2025-12-28 11:12:58 -0500

    Additional tool setup for react

---
+++ .eslintrc.cjs
@@ -0,0 +1,7 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2022: true },
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'react-hooks'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
+};
